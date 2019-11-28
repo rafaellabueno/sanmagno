de6b63a1b93618659b0d873999cd770d2c7eacd7
Guilherme <guimnezes996@gamil.com>
2019-11-28 10:46:33 -0200

    <tela cadastro e tela consulta>
    <tela consulta>

--- src/visao/TelaCadastro.java
+++ src/visao/TelaCadastro.java
@@ -5,8 +5,14 @@ import net.miginfocom.swing.MigLayout;
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JTextField;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.Color;
+import java.awt.FlowLayout;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.SoftBevelBorder;
+import javax.swing.SwingConstants;
+import javax.swing.Icon;
 
 public class TelaCadastro extends JPanel {
 	
@@ -17,54 +23,89 @@ public class TelaCadastro extends JPanel {
 	private JButton btnLimpar;
 	private JButton btnCadastrar;
 	private JLabel lblAviso;
+	private JPanel panel;
+	private JPanel panel_1;
+	private JPanel panel_2;
+	private Object get;
+	private JPanel panel_3;
+	private JLabel lblTelaCadastro;
+	private JLabel label;
+	ImageIcon  imagem = new ImageIcon(getClass().getResource("user.png"));
 	
 
 	/**
 	 * Create the panel.
 	 */
 	public TelaCadastro() {
-		setLayout(new MigLayout("", "[][][][][][][][][][]", "[][][][][][][]"));
+		setBackground(new Color(255, 255, 255));
+		setLayout(new MigLayout("", "[grow][][][][][][][][][]", "[grow][][][grow][][grow][][]"));
 		
-		JLabel lblTelaCadastro = new JLabel("Tela Cadastro");
-		lblTelaCadastro.setForeground(new Color(0, 128, 128));
+		panel = new JPanel();
+		panel.setBackground(new Color(102, 205, 170));
+		add(panel, "cell 0 0 10 1,grow");
+		panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		panel_3 = new JPanel();
+		panel_3.setBackground(new Color(102, 205, 170));
+		panel.add(panel_3);
+		panel_3.setLayout(new MigLayout("", "[]", "[][]"));
+		
+		lblTelaCadastro = new JLabel("Tela Cadastro");
+		lblTelaCadastro.setForeground(new Color(255, 255, 255));
 		lblTelaCadastro.setFont(new Font("Tahoma", Font.BOLD, 25));
-		add(lblTelaCadastro, "cell 0 0");
+		panel_3.add(lblTelaCadastro, "flowy,cell 0 0");
 		
-		JLabel lblNome = new JLabel("Nome: ");
+		label = new JLabel(new ImageIcon(TelaCadastro.class.getResource("/visao/user.png")));
+		label.setHorizontalAlignment(SwingConstants.TRAILING);
+		panel_3.add(label, "cell 0 0,alignx center");
+		
+		panel_1 = new JPanel();
+		panel_1.setBackground(new Color(255, 255, 255));
+		add(panel_1, "cell 0 1 10 4,alignx center,aligny center");
+		panel_1.setLayout(new MigLayout("", "[]", "[][][][][][][][]"));
+		
+		JLabel lblNome = new JLabel("Nome");
+		panel_1.add(lblNome, "cell 0 0");
 		lblNome.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		add(lblNome, "flowx,cell 0 2 2 1,alignx left");
-		
-		JLabel lblDataNas = new JLabel("Data de Nascimento:");
-		lblDataNas.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		add(lblDataNas, "flowx,cell 0 4 2 1");
-		
-		lblAviso = new JLabel("");
-		lblAviso.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		add(lblAviso, "cell 0 5");
-		
-		btnCadastrar = new JButton("Cadastrar");
-		btnCadastrar.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		add(btnCadastrar, "flowx,cell 0 6");
-		
-		btnLimpar = new JButton("Limpar");
-		btnLimpar.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		add(btnLimpar, "cell 0 6");
 		
 		textNome = new JTextField();
-		add(textNome, "cell 0 2 2 1,alignx left,grow");
+		panel_1.add(textNome, "cell 0 1,growx");
 		textNome.setColumns(10);
 		
-		JLabel lblCPF = new JLabel("CPF:");
+		JLabel lblCPF = new JLabel("CPF");
+		panel_1.add(lblCPF, "cell 0 2");
 		lblCPF.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		add(lblCPF, "flowx,cell 0 3 2 1,alignx left");
 		
 		textCPF = new JTextField();
-		add(textCPF, "flowy,cell 0 3 2 1,grow");
+		panel_1.add(textCPF, "cell 0 3,growx");
 		textCPF.setColumns(10);
 		
+		JLabel lblDataNas = new JLabel("Data de Nascimento");
+		panel_1.add(lblDataNas, "cell 0 4");
+		lblDataNas.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		
 		textData = new JTextField();
-		add(textData, "cell 0 4 2 1,grow");
+		panel_1.add(textData, "cell 0 5,growx");
 		textData.setColumns(10);
+		
+		btnCadastrar = new JButton("Cadastrar");
+		panel_1.add(btnCadastrar, "flowx,cell 0 6");
+		btnCadastrar.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		
+		btnLimpar = new JButton("Limpar");
+		panel_1.add(btnLimpar, "cell 0 6");
+		btnLimpar.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		
+		lblAviso = new JLabel("");
+		panel_1.add(lblAviso, "cell 0 7");
+		lblAviso.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		
+		panel_2 = new JPanel();
+		panel_2.setBackground(new Color(102, 205, 170));
+		FlowLayout flowLayout_1 = (FlowLayout) panel_2.getLayout();
+		add(panel_2, "cell 0 5 10 1,grow");
+		
+		
 
 	}
 
--- src/visao/TelaConsulta.java
+++ src/visao/TelaConsulta.java
@@ -6,6 +6,7 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.Color;
 import javax.swing.JTextField;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 public class TelaConsulta extends JPanel {
@@ -18,51 +19,83 @@ public class TelaConsulta extends JPanel {
 	private JLabel lblSenha;
 	private JButton btnVoltar;
 	private JButton btnLimpar;
+	private JPanel panel;
+	private JPanel panel_1;
+	private JPanel panel_2;
+	private JLabel label;
+	
+	ImageIcon  imgPessoas = new ImageIcon(getClass().getResource("pessoas.png"));
+	private JLabel lblFotoPessoas;
+	private JPanel panel_3;
 
 	/**
 	 * Create the panel.
 	 */
 	public TelaConsulta() {
-		setLayout(new MigLayout("", "[][]", "[][][][][][][][][][]"));
-
-		JLabel lblTelaConsulta = new JLabel("Tela Consulta");
-		lblTelaConsulta.setForeground(new Color(0, 128, 128));
-		lblTelaConsulta.setFont(new Font("Tahoma", Font.BOLD, 25));
-		add(lblTelaConsulta, "cell 0 0");
-
-		JLabel lblDigiteOCpf = new JLabel("Digite o CPF do paciente");
-		lblDigiteOCpf.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		add(lblDigiteOCpf, "flowx,cell 0 3");
-
-		textCPF = new JTextField();
-		textCPF.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		add(textCPF, "cell 0 3");
-		textCPF.setColumns(10);
-
-		btnPesquisar = new JButton("Pesquisar");
-		btnPesquisar.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		add(btnPesquisar, "cell 1 3");
-
-		lblPaciente = new JLabel("");
-		lblPaciente.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		add(lblPaciente, "cell 0 5,growx");
-
-		btnVoltar = new JButton("Voltar");
-		btnVoltar.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		add(btnVoltar, "flowx,cell 0 8");
-
-		btnGerarSenha = new JButton("Gerar Senha");
-		btnGerarSenha.setEnabled(false);
-		btnGerarSenha.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		add(btnGerarSenha, "cell 0 8,alignx right");
+		setBackground(new Color(255, 255, 255));
+		setLayout(new MigLayout("", "[grow][]", "[grow][grow][][][][][][][][grow]"));
+		
+		panel = new JPanel();
+		panel.setBackground(new Color(102, 205, 170));
+		add(panel, "cell 0 0 2 1,grow");
+				
+				panel_2 = new JPanel();
+				panel_2.setBackground(new Color(102, 205, 170));
+				panel.add(panel_2);
+				panel_2.setLayout(new MigLayout("", "[]", "[]"));
+				
+				label = new JLabel("Tela Consulta");
+				label.setForeground(new Color(255, 255, 255));
+				label.setFont(new Font("Tahoma", Font.BOLD, 25));
+				panel_2.add(label, "flowy,cell 0 0");
+				
+				lblFotoPessoas = new JLabel(new ImageIcon(TelaConsulta.class.getResource("/visao/pessoas.png")));				
+				lblFotoPessoas.setText("");
+				lblFotoPessoas.setSize(66, 66);
+				panel_2.add(lblFotoPessoas, "cell 0 0,alignx center");
+		
+		panel_1 = new JPanel();
+		panel_1.setBackground(new Color(255, 255, 255));
+		add(panel_1, "cell 0 1 2 6,alignx center,aligny top");
+		panel_1.setLayout(new MigLayout("", "[][]", "[][][]"));
+				
+						JLabel lblDigiteOCpf = new JLabel("Digite o CPF do paciente");
+						panel_1.add(lblDigiteOCpf, "flowx,cell 0 0");
+						lblDigiteOCpf.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		
+				textCPF = new JTextField();
+				panel_1.add(textCPF, "cell 0 0");
+				textCPF.setFont(new Font("Tahoma", Font.PLAIN, 14));
+				textCPF.setColumns(10);
+								
+										btnPesquisar = new JButton("Pesquisar");
+										panel_1.add(btnPesquisar, "cell 0 0 2 1,growx");
+										btnPesquisar.setFont(new Font("Tahoma", Font.PLAIN, 14));
+										
+												lblPaciente = new JLabel("");
+												panel_1.add(lblPaciente, "cell 0 1");
+												lblPaciente.setFont(new Font("Tahoma", Font.PLAIN, 14));
+												
+														btnLimpar = new JButton("Limpar");
+														panel_1.add(btnLimpar, "flowx,cell 0 2");
+														btnLimpar.setFont(new Font("Tahoma", Font.PLAIN, 14));
+														
+																btnVoltar = new JButton("Voltar");
+																panel_1.add(btnVoltar, "cell 0 2,alignx center");
+																btnVoltar.setFont(new Font("Tahoma", Font.PLAIN, 14));
+																
+																		btnGerarSenha = new JButton("Gerar Senha");
+																		panel_1.add(btnGerarSenha, "cell 0 2,alignx center");
+																		btnGerarSenha.setEnabled(false);
+																		btnGerarSenha.setFont(new Font("Tahoma", Font.PLAIN, 14));
 
 		lblSenha = new JLabel("");
+		panel_1.add(lblSenha, "cell 0 2,growx");
 		lblSenha.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		add(lblSenha, "cell 1 8,alignx center");
-
-		btnLimpar = new JButton("Limpar");
-		btnLimpar.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		add(btnLimpar, "cell 0 9");
+		
+		panel_3 = new JPanel();
+		panel_3.setBackground(new Color(102, 205, 170));
+		add(panel_3, "cell 0 9 2 1,grow");
 
 	}
 
